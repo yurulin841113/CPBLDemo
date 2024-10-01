b98fd057b96ef0ea0611820211bcac295cd05419
roger <yurulin841113@gmail.com>
2024-10-01 18:06:42 +0800

    feat:1.新增球隊欄位

--- CPBLDemo/Data/dbCpblBaseBallContext.cs
+++ CPBLDemo/Data/dbCpblBaseBallContext.cs
@@ -21,8 +21,15 @@ public partial class dbCpblBaseBallContext : DbContext
             entity.Property(e => e.CreatedTime).HasColumnType("datetime");
             entity.Property(e => e.FirstDate).HasColumnType("datetime");
             entity.Property(e => e.Height).HasColumnType("decimal(18, 3)");
-            entity.Property(e => e.Name).HasMaxLength(50);
-            entity.Property(e => e.Position).HasMaxLength(50);
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+            entity.Property(e => e.Position)
+                .IsRequired()
+                .HasMaxLength(50);
+            entity.Property(e => e.Team)
+                .IsRequired()
+                .HasMaxLength(50);
             entity.Property(e => e.Weight).HasColumnType("decimal(18, 3)");
         });
 
