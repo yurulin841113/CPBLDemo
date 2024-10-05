108ee22829e617cd115a57d0ad20448ea14b64cf
roger <yurulin841113@gmail.com>
2024-10-06 00:30:32 +0800

    feat:補上其他頁面

---
+++ CPBLDemo/Data/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPBLDemo.Data
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
